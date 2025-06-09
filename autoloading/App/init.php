369922036dd4ac530c1c0928688d39572217ca84
riant57 <riant57@gmail.com>
2025-06-10 06:17:22 +0700

    Belajar autoloading (spl_autoload_register masih error)

---
+++ autoloading/App/init.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once '/opt/lampp/htdocs/oophp/autoloading/App/produk/infoProduk.php';
+require_once '/opt/lampp/htdocs/oophp/autoloading/App/produk/Produk.php';
+require_once '/opt/lampp/htdocs/oophp/autoloading/App/produk/Komik.php';
+require_once '/opt/lampp/htdocs/oophp/autoloading/App/produk/Game.php';
+require_once '/opt/lampp/htdocs/oophp/autoloading/App/produk/CetakInfoProduk.php';
+
+// spl_autoload_register(function($class){
+//     require_once __DIR__.'/produk/' .$class. '.php';
+    
+
+// });
+
+
+
+?>
+
